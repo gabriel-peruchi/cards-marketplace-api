e60e9a34120e78f98b856b4e7b75a1265dc3c8ce
Gabriel Peruchi <gabriel-peruchi@hotmail.com>
2024-02-15 12:05:22 -0300

    feat: list cards

---
+++ src/http/routes/ListCards.ts
@@ -0,0 +1,44 @@
+import { z } from "zod"
+import { prisma } from "../../lib/prisma"
+import { FastifyInstance } from "fastify"
+import { Page } from "../../types/Page"
+
+export async function ListCards(app: FastifyInstance) {
+  app.get('/cards', async (request, reply) => {
+    const listCardQuery = z.object({
+      rpp: z.number(),
+      page: z.number(),
+      search: z.string().optional(),
+    })
+
+    const { page, rpp, search } = listCardQuery.parse(request.query)
+
+    const queryPayload = {
+      take: rpp + 1,
+      skip: (page - 1) * rpp,
+      where: {},
+    }
+
+    if (search) {
+      queryPayload.where = {
+        name: {
+          contains: search,
+          mode: 'insensitive'
+        },
+      }
+    }
+
+    const cards = await prisma.card.findMany({
+      ...queryPayload,
+      orderBy: {
+        name: 'asc'
+      }
+    })
+
+    return reply.send(Page.create({
+      page,
+      rpp,
+      list: cards
+    }))
+  })
+}
---
+++ src/types/Page.ts
@@ -0,0 +1,49 @@
+type CreatePageProps<T> = {
+  list: T[]
+  rpp: number
+  page: number
+}
+
+type RestorePageProps<T> = {
+  list: T[]
+  rpp: number
+  page: number
+  more: boolean
+}
+
+export class Page<T> {
+  list: T[]
+  rpp: number
+  page: number
+  more: boolean
+
+  static create<T>({ page, rpp, list }: CreatePageProps<T>) {
+    const pagination = new Page<T>()
+
+    if (rpp > 0) {
+      pagination.rpp = rpp
+      pagination.more = list.length === rpp + 1
+
+      if (rpp + 1 === list.length) {
+        list.pop()
+      }
+    } else {
+      pagination.rpp = list.length
+      pagination.more = false
+    }
+
+    pagination.page = page
+    pagination.list = list
+
+    return pagination
+  }
+
+  static restore<T>({ page, rpp, more, list }: RestorePageProps<T>) {
+    const pagination = new Page<T>()
+    pagination.list = list
+    pagination.more = more
+    pagination.rpp = rpp
+    pagination.page = page
+    return pagination
+  }
+}
