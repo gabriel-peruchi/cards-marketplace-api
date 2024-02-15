f065ad591c8658bc44c5d43bf012702927f39ea9
Gabriel Peruchi <gabriel-peruchi@hotmail.com>
2024-02-15 12:05:43 -0300

    feat: add on my cards

---
+++ src/http/routes/AddOnMyCards.ts
@@ -0,0 +1,33 @@
+import { z } from "zod"
+import { prisma } from "../../lib/prisma"
+import { FastifyInstance } from "fastify"
+
+export async function AddOnMyCards(app: FastifyInstance) {
+  app.post('/me/cards', async (request, reply) => {
+    const userId = '' // TODO: pegar do contexto
+
+    const addOnMyCards = z.object({
+      cardIds: z.array(z.string().uuid()),
+    })
+
+    const { cardIds } = addOnMyCards.parse(request.body)
+
+    const existingUserCards = await prisma.userCard.findMany({
+      where: {
+        userId
+      },
+    })
+
+    const existingCards = existingUserCards.map(userCard => userCard.cardId)
+
+    const userCards = cardIds
+      .filter(cardId => !existingCards.includes(cardId))
+      .map(cardId => ({ cardId, userId }))
+
+    await prisma.userCard.createMany({
+      data: userCards
+    })
+    
+    return reply.send()
+  })
+}
