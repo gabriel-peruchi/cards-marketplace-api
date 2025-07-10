01030054828f613b65396d324f068dbe680e5b7e
Marcos <m.juniorcsebastiao@gmail.com>
2025-07-10 16:55:25 -0300

    feat: adiciona rota GetCard e remove CreateCard

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,7 +2,6 @@ import fastify from 'fastify'
 import cors from '@fastify/cors'
 
 import { Login } from './routes/Login'
-import { CreateCard } from './routes/CreateCard'
 import { CreateTrade } from './routes/CreateTrade'
 import { DeleteTrade } from './routes/DeleteTrade'
 import { GetProfile } from './routes/GetProfile'
@@ -12,6 +11,7 @@ import { ListTrades } from './routes/ListTrades'
 import { RegisterUser } from './routes/RegisterUser'
 import { AddOnMyCards } from './routes/AddOnMyCards'
 import AuthPlugin from '../lib/auth'
+import { GetCard } from './routes/GetCard'
 
 const app = fastify()
 
@@ -23,9 +23,9 @@ app.register(GetProfile)
 app.register(RegisterUser)
 
 app.register(ListCards)
-app.register(CreateCard)
 app.register(ListMyCards)
 app.register(AddOnMyCards)
+app.register(GetCard)
 
 app.register(ListTrades)
 app.register(CreateTrade)
